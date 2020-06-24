fe072febf7e8d6e62c6b387f01cfbb97815bf688
Eden BERGEL <eb@wshop-cloudcommerce.com>
2020-06-24 19:14:05 +0200

    spritesheet fix bug, inte page defis

--- src/components/Defis/Defis.js
+++ src/components/Defis/Defis.js
@@ -5,6 +5,38 @@ function Defis(){
   return(
     <div className="defis">
       <h1 className="page_title">Choisi une catégorie<br/>pour ton défi du jour</h1>
+
+      <div className="defis_categ_container">
+        <div className="defis_categ_bloc">
+          <div></div>
+          <h2>Alimentation</h2>
+        </div>
+
+        <div className="defis_categ_bloc">
+          <div></div>
+          <h2>Énergie</h2>
+        </div>
+
+        <div className="defis_categ_bloc">
+          <div></div>
+          <h2>Déchets</h2>
+        </div>
+
+        <div className="defis_categ_bloc">
+          <div></div>
+          <h2>Déplacements</h2>
+        </div>
+
+        <div className="defis_categ_bloc">
+          <div></div>
+          <h2>Dyi</h2>
+        </div>
+
+        <div className="defis_categ_bloc">
+          <div></div>
+          <h2>Au travail</h2>
+        </div>
+      </div>
     </div>
   );
 }
