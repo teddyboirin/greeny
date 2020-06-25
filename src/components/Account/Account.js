59637c5613b42b341f0e83f3820caecb0ceedc6f
Eden BERGEL <eb@wshop-cloudcommerce.com>
2020-06-25 16:01:01 +0200

    optimize style & vue account / defis

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -8,13 +8,18 @@ function Account(){
       <h1 className="page_title">Hello</h1>
       <h3>Un nouveau jour, un nouveau défi</h3>
 
-      <Category categ='Alimentation'/>
-      <Category categ='Energie'/>
-      <Category categ='Déchets'/>
-      <Category categ='Déplacements'/>
-      <Category categ='Diy'/>
-      <Category categ='Au travail'/>
+      <div className="account_inner_bloc">
+        <div className="defis_categ_container">
+          <Category categ='Alimentation' points="40pts"/>
+          <Category categ='Energie' points="20pts"/>
+          <Category categ='Déchets' points="12pts"/>
+          <Category categ='Déplacements' points="8pts"/>
+          <Category categ='DIY' points="8pts"/>
+          <Category categ='Au travail' points="15pts"/>
+        </div>
 
+        <div className="bloc_info"></div>
+      </div>
     </div>
   );
 }
--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,10 +1,12 @@
 import React from "react";
+import './category.scss';
 
 function Category(props){
   return(
     <div className="defis_categ_bloc">
       <div></div>
       <h2>{props.categ}</h2>
+      <h1>{props.points}</h1>
     </div>
   );
 }
