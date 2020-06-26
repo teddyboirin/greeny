5ce3fda9a466e1e6dc37589d89eec61ff0c8990c
Eden BERGEL <eb@wshop-cloudcommerce.com>
2020-06-26 11:55:12 +0200

    page compte

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -6,16 +6,25 @@ function Account(){
   return(
     <div className="account">
       <h1 className="page_title">Hello</h1>
-      <h3>Un nouveau jour, un nouveau défi</h3>
+      <h3 className="page_subtitle">Un nouveau jour, un nouveau défi</h3>
 
       <div className="account_inner_bloc">
-        <div className="defis_categ_container">
-          <Category categ='Alimentation' points="40pts"/>
-          <Category categ='Energie' points="20pts"/>
-          <Category categ='Déchets' points="12pts"/>
-          <Category categ='Déplacements' points="8pts"/>
-          <Category categ='DIY' points="8pts"/>
-          <Category categ='Au travail' points="15pts"/>
+
+        <div className="account_inner_right">
+          <div className="account_tab">
+            <h3>Votre évolution</h3>
+            <h3>Vos défis récurrents</h3>
+          </div>
+
+          <div className="defis_categ_container">
+            <Category categ='Alimentation' points="40pts"/>
+            <Category categ='Energie' points="20pts"/>
+            <Category categ='Déchets' points="12pts"/>
+            <Category categ='Déplacements' points="8pts"/>
+            <Category categ='DIY' points="8pts"/>
+            <Category categ='Au travail' points="15pts"/>
+          </div>
+          <h3>Total des points accumulés</h3>
         </div>
 
         <div className="bloc_info"></div>
