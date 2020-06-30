eab116f2e12d834140861746e1d2df9971a4f005
Léa De Amorin <leadeamorin.ps@gmail.com>
2020-06-30 23:22:44 +0200

    [tpl] changed favicon

--- src/components/global/Defi.js
+++ src/components/global/Defi.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import './defi.scss'
 
 function Defi(props){
+
+  const [defiClicked, setDefiClicked] = useState(false)
+
   return(
-    <div className="defi">
+    <div onClick={() => setDefiClicked(true)} className={defiClicked === false ? 'defi' : 'defi_clicked' + ' defi'}>
       <div className='defi_top'>
         <span className="defi_bfr"></span>
         <div>{props.points}pts</div>
